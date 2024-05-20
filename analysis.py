8960ad40fc0c5da0db4f4adf476582e3f0a9a78a
Shuhrat <79278760+EliteCallsYou@users.noreply.github.com>
2024-05-20 23:56:22 +0500

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -6,8 +6,6 @@ from streamlit import session_state as state
 from streamlit_option_menu import option_menu
 from streamlit_extras.dataframe_explorer import dataframe_explorer
 
-import pandas_profiling
-from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
 import matplotlib.pyplot as plt
